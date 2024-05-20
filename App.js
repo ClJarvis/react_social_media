a048f3697bc449764883dd558bd7f4d2274eecf8
Chris <clj@ollure.com>
2024-05-20 11:33:41 -0500

    refactoring post

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Entypo,  AntDesign, FontAwesome5, MaterialCommunityIcons,} from "@expo/vector-icons";
 import LikeImage from './assets/images/like.png';
+import FeedPost from './src/componets/FeedPost';
 
 const post = {
   id: "p1",
@@ -22,64 +23,7 @@ const post = {
 export default function App() {
   return (
     <View style={styles.container}>
-      
-      <View style={styles.post}>
-
-        
-        <View style={styles.header}>
-          <Image source={{ uri: post.User.image }} 
-          style={styles.profileImage}
-          />
-        </View>
-
-        <View>
-          <Text style={styles.name}>{post.User.name}</Text>
-          <Text style={styles.subtitle}>{post.createdAt}</Text>
-        </View>
-         <Entypo
-            name="dots-three-horizontal"
-            size={18}
-            color="gray"
-            style={styles.icon}
-          />
-
-    {post.description && <Text style={styles.description}>{post.description}</Text>
-    }
-    {post.image && (<Image source={{ uri: post.image }} style={styles.image}
-    resizeMode="cover"
-    /> 
-    )}
-
-        <View style={styles.footer}>
-            <View styles={styles.statsRow}>
-              <Image source={LikeImage} style={styles.likeIcon} />
-              <Text style={styles.likedBy}>
-                Tony Stark and {post.numbersOfLikes}others.
-                </Text>
-                <Text style={styles.numberOfShares}>
-                  {post.numberOfShares} shares
-                </Text>
-            </View>
-
-            <View style={styles.buttonsRow}>
-                <View style={styles.iconButton}>
-                  <AntDesign name="like2" size={18}  color="grey" />
-                  <Text style={styles.iconButtonText}>Like</Text>
-                </View>
-           
-
-                <View style={styles.iconButton}>
-                  <FontAwesome5 name="comment-alt" size={18}  color="grey" />
-                  <Text style={styles.iconButtonText}>comment</Text>
-                </View>
-
-                <View style={styles.iconButton}>
-                  <MaterialCommunityIcons name="share-outline" size={18}  color="grey" />
-                  <Text style={styles.iconButtonText}>Share</Text>
-                </View>
-            </View>
-          </View>
-        </View>
+      <FeedPost />
 
       <StatusBar style="auto" />
     </View>
